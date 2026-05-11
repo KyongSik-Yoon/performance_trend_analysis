5273324821038bfb314693279c500c5abdefbb44
novalove <juhyun.lee@jennifersoft.com>
2026-05-11 11:19:40 +0900

    config.js 설정 수정

--- public/pta/config.js
+++ public/pta/config.js
@@ -1,10 +1,10 @@
 // 修正後ブラウザをリフレッシュすると変更事項が反映されます。
 window.PTA_CONFIG = {
   // 1. API Proxy用ベースURL (Proxy設定用)
-  BASE_URL: 'https://13.158.36.15:8443/',
+  BASE_URL: 'https://demo.jennifersoft.co.jp/',
 
   // 2. APIサーバーのドメインまたはIP (Nginx等でプロキシを使用する場合は空にしておく、直接呼び出す場合はアドレスを入力)
-  API_DOMAIN: '',
+  API_DOMAIN: 'https://demo.jennifersoft.co.jp/',
 
   // 3. API認証に使用されるJENNIFER OpenAPIトークン
   TOKEN: 'd5HJuONXNZf'
